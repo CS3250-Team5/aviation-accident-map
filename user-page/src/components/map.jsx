cbb6ce0982fa9b0000c71039cd3a374b272089ac
beavelar <bavelar@msudenver.edu>
2018-10-01 17:00:57 -0600

    Implementing google maps

--- user-page/src/components/map.jsx
+++ user-page/src/components/map.jsx
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
-import GoogleMapReact from 'google-map-react';
-import './App.css'
+import React, { Component } from "react";
+import GoogleMapReact from "google-map-react";
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
@@ -16,15 +15,19 @@ class Map extends Component {
   render() {
     return (
       // Important! Always set the container height explicitly
-      <div style={{ height: '50vh', width: '60%', margin: '0 auto', padding: '100px 0 0 0'}}>
+      <div
+        style={{
+          height: "50vh",
+          width: "60%",
+          margin: "0 auto",
+          padding: "100px 0 0 0"
+        }}
+      >
         <GoogleMapReact
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
-          <AnyReactComponent
-            lat={39.73}
-            lng={-104.99}
-          />
+          <AnyReactComponent lat={39.73} lng={-104.99} />
         </GoogleMapReact>
       </div>
     );
