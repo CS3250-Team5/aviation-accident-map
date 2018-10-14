de57016d1227b84bc29df6c927c6070f583cf1c9
Francisco Duran <Franciscoduran93@gmail.com>
2018-10-14 12:41:00 -0600

    addition of map markers

--- user-page/src/components/map.jsx
+++ user-page/src/components/map.jsx
@@ -4,7 +4,8 @@ import GoogleMapReact from "google-map-react";
 import plane from "../images/ge_crash.png";
 
 //const AnyReactComponent
-const fatalAccidents = ({ text }) => <div><img src={plane}/>{text}</div>;
+//conts have to be uppercase
+const FatalAccidents = ({ text }) => <div className="fatal"><img src={plane}/>{text}</div>;
 
 class Map extends Component {
   static defaultProps = {
@@ -31,17 +32,17 @@ class Map extends Component {
           defaultZoom={this.props.zoom}
         >
 
-          <fatalAccidents lat={39.6290176} lng={-105.0920324} />
-          <fatalAccidents lat={39.516111} lng={-104.833889} />
-          <fatalAccidents lat={39.46} lng={-105.663333} />
-          <fatalAccidents lat={39.41} lng={-107.21} />
-          <fatalAccidents lat={40.267223} lng={-105.154167} />
-          <fatalAccidents lat={38.8625} lng={-106.156944} />
-          <fatalAccidents lat={37.822223} lng={-106.906111} />
-          <fatalAccidents lat={38.495} lng={-102.29} />
-          <fatalAccidents lat={39.1175} lng={-104.718334} />
-          <fatalAccidents lat={40.052222} lng={-108.278611} />
-          <fatalAccidents lat={38.830834} lng={-104.718334} />
+          <FatalAccidents lat={39.6290176} lng={-105.0920324} />
+          <FatalAccidents lat={39.516111} lng={-104.833889} />
+          <FatalAccidents lat={39.46} lng={-105.663333} />
+          <FatalAccidents lat={39.41} lng={-107.21} />
+          <FatalAccidents lat={40.267223} lng={-105.154167} />
+          <FatalAccidents lat={38.8625} lng={-106.156944} />
+          <FatalAccidents lat={37.822223} lng={-106.906111} />
+          <FatalAccidents lat={38.495} lng={-102.29} />
+          <FatalAccidents lat={39.1175} lng={-104.718334} />
+          <FatalAccidents lat={40.052222} lng={-108.278611} />
+          <FatalAccidents lat={38.830834} lng={-104.718334} />
 
         </GoogleMapReact>
       </div>
