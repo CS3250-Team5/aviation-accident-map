a29776f04cbd385b6ddc0f87613b28eaf8714cd4
Francisco Duran <Franciscoduran93@gmail.com>
2018-10-14 18:12:40 -0600

    markers into props

--- user-page/src/components/map.jsx
+++ user-page/src/components/map.jsx
@@ -12,6 +12,52 @@ const FatalAccidents = ({ text }) => <div><img src={plane}/>{text}</div>;
 
 class Map extends Component {
   static defaultProps = {
+      markers: {
+        one: {
+          lat: 39.6290176,
+          lng: -105.0920324
+        },
+        two: {
+          lat: 39.516111,
+          lng: -104.833889
+        },
+        three: {
+          lat: 39.46,
+          lng: -105.663333
+        },
+        four: {
+          lat: 39.41,
+          lng: -107.21
+        },
+        five: {
+          lat: 40.267223,
+          lng: -105.154167
+        },
+        six: {
+          lat: 38.8625,
+          lng: -106.156944
+        },
+        seven: {
+          lat: 37.822223,
+          lng: -106.906111
+        },
+        eight: {
+          lat: 38.495,
+          lng: -102.29
+        },
+        nine: {
+          lat: 39.1175,
+          lng: -104.718334
+        },
+        ten: {
+          lat: 40.052222,
+          lng: -108.278611
+        },
+        eleven: {
+          lat: 38.830834,
+          lng: -104.718334
+        }
+    },
     center: {
       lat: 39.73,
       lng: -104.99
@@ -66,25 +112,60 @@ class Map extends Component {
           padding: "50px 0 0 0"
         }}
       >
+
         <GoogleMapReact
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
 
-          <FatalAccidents lat={39.6290176} lng={-105.0920324} />
-          <FatalAccidents lat={39.516111} lng={-104.833889} />
-          <FatalAccidents lat={39.46} lng={-105.663333} />
-          <FatalAccidents lat={39.41} lng={-107.21} />
-          <FatalAccidents lat={40.267223} lng={-105.154167} />
-          <FatalAccidents lat={38.8625} lng={-106.156944} />
-          <FatalAccidents lat={37.822223} lng={-106.906111} />
-          <FatalAccidents lat={38.495} lng={-102.29} />
-          <FatalAccidents lat={39.1175} lng={-104.718334} />
-          <FatalAccidents lat={40.052222} lng={-108.278611} />
-          <FatalAccidents lat={38.830834} lng={-104.718334} />
+        <FatalAccidents
+          lat={this.props.markers.one.lat}
+          lng={this.props.markers.one.lng}
+        />
+        <FatalAccidents
+          lat={this.props.markers.two.lat}
+          lng={this.props.markers.two.lng}
+        />
+        <FatalAccidents
+          lat={this.props.markers.three.lat}
+          lng={this.props.markers.three.lng}
+        />
+        <FatalAccidents
+          lat={this.props.markers.four.lat}
+          lng={this.props.markers.four.lng}
+        />
+        <FatalAccidents
+          lat={this.props.markers.five.lat}
+          lng={this.props.markers.five.lng}
+        />
+        <FatalAccidents
+          lat={this.props.markers.six.lat}
+          lng={this.props.markers.six.lng}
+        />
+        <FatalAccidents
+          lat={this.props.markers.seven.lat}
+          lng={this.props.markers.seven.lng}
+        />
+        <FatalAccidents
+          lat={this.props.markers.eight.lat}
+          lng={this.props.markers.eight.lng}
+        />
+        <FatalAccidents
+          lat={this.props.markers.nine.lat}
+          lng={this.props.markers.nine.lng}
+        />
+        <FatalAccidents
+          lat={this.props.markers.ten.lat}
+          lng={this.props.markers.ten.lng}
+        />
+        <FatalAccidents
+          lat={this.props.markers.eleven.lat}
+          lng={this.props.markers.eleven.lng}
+        />
 
         </GoogleMapReact>
-      </div></div>
+      </div>
+    </div>
     );
   }
 }
