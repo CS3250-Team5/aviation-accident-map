69d28e25f8e590c7fc268fe02b76b43bf5110672
Francisco Duran <Franciscoduran93@gmail.com>
2018-10-15 20:39:50 -0600

    Moved data to states

--- user-page/src/components/map.jsx
+++ user-page/src/components/map.jsx
@@ -14,81 +14,55 @@ const FatalAccidents = ({text}) => <div><img src={plane} alt="plane"/>{text}</di
 const MountainPasses = ({text}) => <div><img src={mountain} alt="mtnPass"/>{text}</div>;
 
 class Map extends Component {
+
+    state={
+        fatalOne: {
+            lat: 39.6290176,
+            lng: -105.0920324
+        },
+        fatalTwo: {
+            lat: 39.516111,
+            lng: -104.833889
+        },
+        fatalThree: {
+            lat: 39.46,
+            lng: -105.663333
+        },
+        fatalFour: {
+            lat: 39.41,
+            lng: -107.21
+        },
+        fatalFive: {
+            lat: 40.267223,
+            lng: -105.154167
+        },
+        fatalSix: {
+            lat: 38.8625,
+            lng: -106.156944
+        },
+        fatalSeven: {
+            lat: 37.822223,
+            lng: -106.906111
+        },
+        fatalEight: {
+            lat: 38.495,
+            lng: -102.29
+        },
+        fatalNine: {
+            lat: 39.1175,
+            lng: -104.718334
+        },
+        fatalTen: {
+            lat: 40.052222,
+            lng: -108.278611
+        },
+        fatalEleven: {
+            lat: 38.830834,
+            lng: -104.718334
+        }
+    };
+
     static defaultProps = {
-        accidents: {
-            one: {
-                lat: 39.6290176,
-                lng: -105.0920324
-            },
-            two: {
-                lat: 39.516111,
-                lng: -104.833889
-            },
-            three: {
-                lat: 39.46,
-                lng: -105.663333
-            },
-            four: {
-                lat: 39.41,
-                lng: -107.21
-            },
-            five: {
-                lat: 40.267223,
-                lng: -105.154167
-            },
-            six: {
-                lat: 38.8625,
-                lng: -106.156944
-            },
-            seven: {
-                lat: 37.822223,
-                lng: -106.906111
-            },
-            eight: {
-                lat: 38.495,
-                lng: -102.29
-            },
-            nine: {
-                lat: 39.1175,
-                lng: -104.718334
-            },
-            ten: {
-                lat: 40.052222,
-                lng: -108.278611
-            },
-            eleven: {
-                lat: 38.830834,
-                lng: -104.718334
-            }
-        },
-
-        passes: {
-            one: {
-                lat: 39.798383,
-                lng: -105.777125
-            },
-            two: {
-                lat: 40.520833,
-                lng: -105.8925
-            },
-            three: {
-                lat: 38.163007,
-                lng: -106.600192
-            },
-            four: {
-                lat: 39.530278,
-                lng: -107.058889
-            },
-            five: {
-                lat: 38.826666,
-                lng: -106.408611
-            }
-        },
-
-        reset: {
-            none: {lat: null, lng: null}
-        },
-
         center: {
             lat: 39.00,
             lng: -105.15
@@ -111,7 +85,7 @@ class Map extends Component {
             </center>
             <center>
                 <span className="filterBoxes">
-                    <input onClick={this.handleAccidents} name="Fatal Accidents" type="checkbox"/>
+                    <input onClick={this.handleAccidents} name="Fatal Accidents" value="true" type="checkbox"/>
                     <img src={plane} alt="plane" width="15" height="15"></img>Show Fatal Accidents
                 </span>
                 <span className="filterBoxes">
@@ -128,24 +102,17 @@ class Map extends Component {
 
                 <GoogleMapReact defaultCenter={this.props.center} defaultZoom={this.props.zoom}>
 
-                    <FatalAccidents lat={this.props.accidents.one.lat} lng={this.props.accidents.one.lng}/>
-                    <FatalAccidents lat={this.props.accidents.two.lat} lng={this.props.accidents.two.lng}/>
-                    <FatalAccidents lat={this.props.accidents.three.lat} lng={this.props.accidents.three.lng}/>
-                    <FatalAccidents lat={this.props.accidents.four.lat} lng={this.props.accidents.four.lng}/>
-                    <FatalAccidents lat={this.props.accidents.five.lat} lng={this.props.accidents.five.lng}/>
-                    <FatalAccidents lat={this.props.accidents.six.lat} lng={this.props.accidents.six.lng}/>
-                    <FatalAccidents lat={this.props.accidents.seven.lat} lng={this.props.accidents.seven.lng}/>
-                    <FatalAccidents lat={this.props.accidents.eight.lat} lng={this.props.accidents.eight.lng}/>
-                    <FatalAccidents lat={this.props.accidents.nine.lat} lng={this.props.accidents.nine.lng}/>
-                    <FatalAccidents lat={this.props.accidents.ten.lat} lng={this.props.accidents.ten.lng}/>
-                    <FatalAccidents lat={this.props.accidents.eleven.lat} lng={this.props.accidents.eleven.lng}/>
-
-                    <MountainPasses lat={null} lng={null}/>
-                    <MountainPasses lat={this.props.passes.one.lat} lng={this.props.passes.one.lng}/>
-                    <MountainPasses lat={this.props.passes.two.lat} lng={this.props.passes.two.lng}/>
-                    <MountainPasses lat={this.props.passes.three.lat} lng={this.props.passes.three.lng}/>
-                    <MountainPasses lat={this.props.passes.four.lat} lng={this.props.passes.four.lng}/>
-                    <MountainPasses lat={this.props.passes.five.lat} lng={this.props.passes.five.lng}/>
+                    <FatalAccidents lat={this.state.fatalOne.lat} lng={this.state.fatalOne.lng}/>
+                    <FatalAccidents lat={this.state.fatalTwo.lat} lng={this.state.fatalTwo.lng}/>
+                    <FatalAccidents lat={this.state.fatalThree.lat} lng={this.state.fatalThree.lng}/>
+                    <FatalAccidents lat={this.state.fatalFour.lat} lng={this.state.fatalFour.lng}/>
+                    <FatalAccidents lat={this.state.fatalFive.lat} lng={this.state.fatalFive.lng}/>
+                    <FatalAccidents lat={this.state.fatalSix.lat} lng={this.state.fatalSix.lng}/>
+                    <FatalAccidents lat={this.state.fatalSeven.lat} lng={this.state.fatalSeven.lng}/>
+                    <FatalAccidents lat={this.state.fatalEight.lat} lng={this.state.fatalEight.lng}/>
+                    <FatalAccidents lat={this.state.fatalNine.lat} lng={this.state.fatalNine.lng}/>
+                    <FatalAccidents lat={this.state.fatalTen.lat} lng={this.state.fatalTen.lng}/>
+                    <FatalAccidents lat={this.state.fatalEleven.lat} lng={this.state.fatalEleven.lng}/>
 
                 </GoogleMapReact>
             </div>
