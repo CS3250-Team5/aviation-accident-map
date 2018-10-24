8bbad3867b348fa15269234a3069abdda61dff42
beavelar <bavelar@msudenver.edu>
2018-10-24 15:02:02 -0600

    Seperates data from js file

--- user-page/src/reader.js
+++ user-page/src/reader.js
@@ -1,5 +1,7 @@
-// How can you read a file and put data from file to this array?
-var reader = [
+import "./eventIDs.txt";
+
+// Testing purposes only
+var eventIDs = [
   "20160620X21154",
   "20160518X42840",
   "20160302X14248",
@@ -7,13 +9,13 @@ var reader = [
   "20151109X40213"
 ];
 
-// Testing purposes only
+// How can you read a file and put data from file to this array?
 var ids = [];
 
 function setIDs() {
   // i < 5 for this specific case, while loop may be used in the end
   for (var i = 0; i < 5; i++) {
-    ids[i] = reader[i];
+    ids[i] = eventIDs[i];
   }
 }
 
