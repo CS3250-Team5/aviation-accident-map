2ddc3f86f3fecab111b819375184996027878b84
beavelar <bavelar@msudenver.edu>
2018-10-31 18:52:20 -0600

    Revert "Attempt at creating elements dynamically"
    This reverts commit 7dbd4d844710c4328b12cd3b84b565e4c5e6ee3d.

--- user-page/src/components/map.jsx
+++ user-page/src/components/map.jsx
@@ -82,21 +82,6 @@ class Map extends Component {
     zoom: 7
   };
 
-  creatingElements() {
-    return <div>Testing Stuff</div>;
-  }
-
-  testingStuff() {
-    console.log("Entered testingStuff");
-    let testing = [];
-
-    for (var i = 0; i < 10; i++) {
-      testing.push(<div>Testing</div>);
-    }
-
-    return testing;
-  }
-
   handleRandom = () => {
     const latMin = 38;
     const latMax = 41;
@@ -266,8 +251,6 @@ class Map extends Component {
       // Important! Always set the container height explicitly
       <div className="backGround">
         <center>
-          {this.creatingElements()}
-          {this.testingStuff()}
           <span className="filterBoxes">
             <input
               onClick={this.handleRandom}
