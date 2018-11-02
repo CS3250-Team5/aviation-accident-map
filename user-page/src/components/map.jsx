33458de04b8c1d6c33bf06550e06518f1c92a661
beavelar <bavelar@msudenver.edu>
2018-11-01 18:30:17 -0600

    Makes points more dynamic

--- user-page/src/components/map.jsx
+++ user-page/src/components/map.jsx
@@ -22,60 +22,10 @@ const FatalAccidents = ({ lat, lng, link }) => (
   </div>
 );
 
-// only fatalSix to fatalTen have the link sub State
 class Map extends Component {
   state = {
     fatalBox: false,
-    randomBox: false,
-    parse: 0,
-    parser: 0,
-    longitude: null,
-    latitude: null,
-    fatalOne: {
-      lat: null,
-      lng: null
-    },
-    fatalTwo: {
-      lat: null,
-      lng: null
-    },
-    fatalThree: {
-      lat: null,
-      lng: null
-    },
-    fatalFour: {
-      lat: null,
-      lng: null
-    },
-    fatalFive: {
-      lat: null,
-      lng: null
-    },
-    fatalSix: {
-      lat: null,
-      lng: null,
-      link: null
-    },
-    fatalSeven: {
-      lat: null,
-      lng: null,
-      link: null
-    },
-    fatalEight: {
-      lat: null,
-      lng: null,
-      link: null
-    },
-    fatalNine: {
-      lat: null,
-      lng: null,
-      link: null
-    },
-    fatalTen: {
-      lat: null,
-      lng: null,
-      link: null
-    }
+    randomBox: false
   };
 
   static defaultProps = {
@@ -90,6 +40,7 @@ class Map extends Component {
     var j = 0;
     var latP = [];
     var lngP = [];
+    var eventID = [];
     let points = [];
     var testRef = null;
 
@@ -99,51 +50,37 @@ class Map extends Component {
       .child("ROWS")
       .child("ROW");
 
-    for (var i = 0; i < 10; i++) {
+    for (var i = 0; i < 100; i++) {
       testRef = rootRef.child(i).child("Latitude");
       // eslint-disable-next-line
       testRef.on("value", snap => {
         latP[i] = snap.val();
       });
-      //console.log(latP[i]);
     }
 
-    for (i = 0; i < 10; i++) {
+    for (i = 0; i < 100; i++) {
       testRef = rootRef.child(i).child("Longitude");
       // eslint-disable-next-line
       testRef.on("value", snap => {
         lngP[i] = snap.val();
-        //console.log(lngP[i]);
       });
     }
 
-    for (i = 0; i < 10; i++) {
-      var temp1 = lngP[i];
-      var temp2 = latP[i];
-      //console.log(lngP[j]);
-      //this.state.longitude = lngP[i];
-      //this.state.latitude = latP[i];
+    for (i = 0; i < 100; i++) {
+      testRef = rootRef.child(i).child("EventId");
+      // eslint-disable-next-line
+      testRef.on("value", snap => {
+        eventID[i] =
+          "https://app.ntsb.gov/pdfgenerator/ReportGeneratorFile.ashx?EventID=" +
+          snap.val() +
+          "&AKey=1&RType=HTML&IType=FA";
+      });
+    }
 
-      this.setState(
-        {
-          longitude: temp1,
-          latitude: temp2,
-          parser: j++
-        },
-        function() {
-          console.log(this.state.longitude);
-        }
-      );
-      console.log(this.state.longitude);
-      //var lati = latP[i].toString();
-      //var long = lngP[i].toString();
+    for (i = 0; i < 100; i++) {
       points.push(
-        <FatalAccidents lat={this.state.latitude} lng={this.state.longitude} />
+        <FatalAccidents lat={latP[i]} lng={lngP[i]} link={eventID[i]} />
       );
-      //console.log(latP[i]);
-      //console.log(lngP[i]);
-      //console.log(typeof lngP[i]);
-      //console.log(typeof latP[i]);
     }
 
     return points;
@@ -160,72 +97,13 @@ class Map extends Component {
     if (this.state.fatalBox === false) {
       parser = 0;
       this.setState({
-        fatalBox: true,
-        parse: 0,
-        fatalSix: {
-          lat: 37.822223,
-          lng: -106.906111,
-          link: beginUrl + ids[parser] + endUrl
-        },
-        // eslint-disable-next-line
-        parse: parser++,
-        fatalSeven: {
-          lat: 38.495,
-          lng: -102.29,
-          link: beginUrl + ids[parser] + endUrl
-        },
-        // eslint-disable-next-line
-        parse: parser++,
-        fatalEight: {
-          lat: 39.1175,
-          lng: -104.91,
-          link: beginUrl + ids[parser] + endUrl
-        },
-        // eslint-disable-next-line
-        parse: parser++,
-        fatalNine: {
-          lat: 40.052222,
-          lng: -108.278611,
-          link: beginUrl + ids[parser] + endUrl
-        },
-        // eslint-disable-next-line
-        parse: parser++,
-        fatalTen: {
-          lat: 38.830834,
-          lng: -104.718334,
-          link: beginUrl + ids[parser] + endUrl
-        }
+        fatalBox: true
       });
     }
 
     if (this.state.fatalBox === true) {
       this.setState({
-        fatalBox: false,
-        fatalSix: {
-          lng: null,
-          lat: null,
-          link: null
-        },
-        fatalSeven: {
-          lng: null,
-          lat: null,
-          link: null
-        },
-        fatalEight: {
-          lng: null,
-          lat: null,
-          link: null
-        },
-        fatalNine: {
-          lng: null,
-          lat: null,
-          link: null
-        },
-        fatalTen: {
-          lng: null,
-          lat: null,
-          link: null
-        }
+        fatalBox: false
       });
     }
   };
@@ -269,57 +147,15 @@ class Map extends Component {
 
         <div className="mapStyle">
           <GoogleMapReact
-            bootstrapURLKeys={{
-              key: process.env.REACT_APP_API_KEY
-            }}
+            bootstrapURLKeys={
+              {
+                //key: process.env.REACT_APP_API_KEY
+              }
+            }
             defaultCenter={this.props.center}
             defaultZoom={this.props.zoom}
           >
-            <FatalAccidents
-              lat={this.state.fatalOne.lat}
-              lng={this.state.fatalOne.lng}
-            />
-            <FatalAccidents
-              lat={this.state.fatalTwo.lat}
-              lng={this.state.fatalTwo.lng}
-            />
-            <FatalAccidents
-              lat={this.state.fatalThree.lat}
-              lng={this.state.fatalThree.lng}
-            />
-            <FatalAccidents
-              lat={this.state.fatalFour.lat}
-              lng={this.state.fatalFour.lng}
-            />
-            <FatalAccidents
-              lat={this.state.fatalFive.lat}
-              lng={this.state.fatalFive.lng}
-            />
-            <FatalAccidents
-              lat={this.state.fatalSix.lat}
-              lng={this.state.fatalSix.lng}
-              link={this.state.fatalSix.link}
-            />
-            <FatalAccidents
-              lat={this.state.fatalSeven.lat}
-              lng={this.state.fatalSeven.lng}
-              link={this.state.fatalSeven.link}
-            />
-            <FatalAccidents
-              lat={this.state.fatalEight.lat}
-              lng={this.state.fatalEight.lng}
-              link={this.state.fatalEight.link}
-            />
-            <FatalAccidents
-              lat={this.state.fatalNine.lat}
-              lng={this.state.fatalNine.lng}
-              link={this.state.fatalNine.link}
-            />
-            <FatalAccidents
-              lat={this.state.fatalTen.lat}
-              lng={this.state.fatalTen.lng}
-              link={this.state.fatalTen.link}
-            />
+            {this.creatingElements()}
           </GoogleMapReact>
         </div>
       </div>
