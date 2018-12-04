3f02db5de70616541608b5e1cb565890281b24e0
Francisco Duran <Franciscoduran93@gmail.com>
2018-12-03 22:05:59 -0700

    spacing, addition of noopener noreferrer

--- user-page/src/components/about.jsx
+++ user-page/src/components/about.jsx
@@ -4,21 +4,62 @@ import "../style/map.css";
 class About extends Component {
   render() {
     return (
-        <div className="tooltip aboutStyle">
-            <center>
-                <span className="about">About</span>
-                <span className="tooltiptext">
-                    <span>This page created by: </span>
-                    <a href="https://github.com/CS3250-Team5" target="_blank">Team 5<br/></a>
-                    <a href="https://github.com/beavelar" target="_blank">Brian Avelar<br/></a>
-                    <a href="https://github.com/devond5" target="_blank">Devon Despain<br/></a>
-                    <a href="https://github.com/Monce32" target="_blank">Monce Romero<br/></a>
-                    <a href="https://github.com/Fduranji" target="_blank">Francisco Duran<br/></a>
-                    <a href="https://github.com/MuchoE" target="_blank">Angel Cecena<br/></a>
-                    <i />
-                </span>
-            </center>
-        </div>
+      <div className="tooltip aboutStyle">
+        <center>
+          <span className="about">About</span>
+          <span className="tooltiptext">
+            <span>This page created by: </span>
+            <a
+              href="https://github.com/CS3250-Team5"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Team 5<br />
+            </a>
+            <a
+              href="https://github.com/beavelar"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Brian Avelar
+              <br />
+            </a>
+            <a
+              href="https://github.com/devond5"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Devon Despain
+              <br />
+            </a>
+            <a
+              href="https://github.com/Monce32"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Monce Romero
+              <br />
+            </a>
+            <a
+              href="https://github.com/Fduranji"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Francisco Duran
+              <br />
+            </a>
+            <a
+              href="https://github.com/MuchoE"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Angel Cecena
+              <br />
+            </a>
+            <i />
+          </span>
+        </center>
+      </div>
     );
   }
 }
